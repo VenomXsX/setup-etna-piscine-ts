21b809b3252371b70599a23f864ae84aa23d4296
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2023-09-24 21:36:41 +0200

    update setup.sh interactif

--- setup.sh
+++ setup.sh
@@ -1,30 +1,126 @@
 #!/bin/bash
-# Script pour setup votre dossier et clone automatiquement le repo pour la piscine TypeScript
-# Utilisation: bash setup.sh [le-lien-pour-clone] [nom-du-dossier-a-renommer]
 
-cloneURL=$1
-title=$2
+help() {
+printf "\n\t\033[1;32mScript to init a Typescript project\033[0m\n"
+printf "\nusage: <init|help> <project_name?> <url_of_git_repo?>\n"
+printf "\n?: means optional\n\n"
+printf "init\t\t\twill initialize the Typescript project\n"
+printf "<project_name?>\t\twill init in a new dir (default: current dir '.')\n"
+printf "<url_of_git_repo?>\tclone the repo with and rename if with <project_name>\n"
+}
 
-if [ -z $cloneURL ]; then
-	printf "\nVeuillez inclure \033[1;32mle lien du repo\033[0m à cloner en 1er argument\n"
-	exit 1
+init_files() {
+# init 'package.json'
+echo \
+'{
+  "name": "typescript",
+  "version": "1.0.0",
+  "description": "Example package.json for the ETNA TS pool",
+  "main": "build/index.js",
+  "type": "module",
+  "scripts": {
+    "dev": "tsc && node .",
+    "build": "tsc",
+    "start": "node .",
+    "lint": "eslint --fix"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "eslint-config-airbnb-base": "^15.0.0",
+    "eslint-config-airbnb-typescript": "^17.0.0",
+    "readline-sync": "^1.4.10"
+  },
+  "devDependencies": {
+    "@types/readline-sync": "^1.4.4",
+    "@types/node": "^20.6.3"
+  }
+}' \
+>$name/package.json
+
+# init 'tsconfig.json'
+echo \
+'{
+"compilerOptions": {
+"target": "es2016",
+"module": "NodeNext",
+"resolveJsonModule": true,
+"outDir": "build",
+"preserveConstEnums": true,
+"esModuleInterop": true,
+"forceConsistentCasingInFileNames": true,
+"strict": true,
+"skipLibCheck": true
+},
+"include": ["src"],
+"exclude": ["node_modules", "build"]
+}' \
+>$name/tsconfig.json
+
+# init '.eslintrc.json'
+echo \
+'{
+"extends": ["airbnb-base", "airbnb-typescript/base"],
+"parser": "@typescript-eslint/parser",
+"parserOptions": { "project": ["./tsconfig.json"] },
+"plugins": ["@typescript-eslint"],
+"rules": {
+"no-console": "off",
+"no-restricted-syntax": "off",
+"no-param-reassign": "off"
+},
+"ignorePatterns": ["build", "node_modules"]
+}
+' \
+>$name/.eslintrc.json
+
+# init '.gitignore'
+echo \
+'node_modules/
+build/' \
+>$name/.gitignore
+
+# init src/index.ts
+mkdir -p $name/src
+echo 'console.log("hello world");' >$name/src/index.ts
+}
+
+if [ -z $1 ] || [ $1 == "help" ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
+help
+exit 1
 fi
 
-if [ -z $title ]; then
-	printf "\nVeuillez entrer \033[1;32mun nom\033[0m en 2e argument\n"
-	exit 1
+if [ $1 != "init" ]; then
+printf "\nUnknown argument: \033[1;36m$1\033[0m\nPlease use \033[1;36mhelp\033[0m to see more details:\n\n\t"
+printf "\033[1;36mbash setup.sh help\033[0m\n"
+exit 1
 fi
 
-git clone $cloneURL $title
+if [ -z $2 ]; then
+name="."
+else
+name=$2
+fi
 
-cp ./setup_src/* ./$title
+if [ ! -z $3 ]; then
+git clone $3 $name
+fi
 
-cd $title
-mkdir src/
-echo "console.log('Hello world');" > src/index.ts
-npm install
+mkdir -p $name
 
-printf "\n\033[1;32mInstallation terminé\033[0m\n"
-printf "\nExecuter la commande suivante:\n"
-printf '\n\033[1;36mcd "$title"\033[0m\n'
-printf '\n\033[1;36mnpm run dev\033[0m\n'
+init_files
+
+if [ "$name" != '.' ]; then
+cd $name
+fi
+
+npm i
+
+# final output
+printf "\n\033[1;36mInstall complete!\033[0m\n"
+printf "\nRun the command to start:\n\n\t"
+if [ "$name" != '.' ]; then
+printf "\033[1;36mcd $name\033[0m\n\t"
+fi
+printf "\033[1;36mnpm run dev\033[0m\n"
+
