30d4bb1cd3454941df9914e4eadf56842b20d019
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2023-09-24 15:01:37 +0200

    Added (incomplete) error handling in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,16 @@
 title=$1
 cloneURL=$2
 
+if [ -z $cloneURL ]; then
+	echo
+	echo "Veuillez inclure le lien du repo à cloner"
+	exit 1
+else if [ $cloneURL != "git@rendu-git.etna-alternance.net"* ]; then
+	echo
+	echo "Le lien à cloner semble d'être incorrect"
+	exit 1
+fi
+
 git clone $cloneURL $title
 cp ./setup_src/.gitignore ./setup_src/linter.sh ./setup_src/package.json ./setup_src/package-lock.json ./setup_src/.eslintrc.json ./setup_src/tsconfig.json ./$title
 cd $title
